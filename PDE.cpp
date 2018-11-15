6b54af1424dffb1c12130f5dee59c59bfa9c7576
Diana Jimenez <dm.jimenez10@uniandes.edu.co>
2018-11-14 21:14:37 -0500

    funciona

--- PDE.cpp
+++ PDE.cpp
@@ -4,61 +4,55 @@
 #include <fstream>
 using namespace std;
 
-#define N 501
+#define N 200
 
 int main(){
-   double k=1620;
-   double c=820000000;
-   double rho=0.00000271;
-   double v=k/(c*rho);
-   double r=50;
+  double k=1.62;
+  double c=820;
+  double rho=2710;
+  double v=k/(c*rho);
+  double L=0.5;
+  double r=0.05;
+  double dx=L/N;
+  double dy=dx;
+  double dt=(dx*dx*dy*dy)/(2.0*v*(dx*dx+dy*dy));
 
-//Posiciones x y y.
-   double x[N];
-   double y[N];
-   x[0]=0.0;
-   y[0]=0.0;
-   for(int i=1;i<N;i++){
-      x[i]=x[i-1]+1;
-      y[i]=y[i-1]+1;
-   }
+  double presente[N][N];
+  double futuro[N][N];
+  double m[N][N];
 
 //Condicion inicial.
-   double presente[N][N];
-   double futuro[N][N];
-   double dt=1/(2.0*v);
-   for(int i=0;i<N;i++){
-     for(int j=0;j<N;j++){
-       if(((x[i] - 250) * (x[i] - 250) + (y[j] - 250) * (y[j] - 250)) < r * r){
-         presente[j][i]=100.0;
-         futuro[j][i]=presente[j][i];
-       }
-       else if(((x[i] - 250) * (x[i] - 250) + (y[j] - 250) * (y[j] - 250)) == r * r){
-         presente[j][i]=10.0;
-         futuro[j][i]=presente[j][i];
-       }
-       else{
-         presente[j][i]=10.0;
-         futuro[j][i]=presente[j][i];
-       }
-     }
-   }
+  for(int i=0;i<N;i++){
+    for(int j=0;j<N;j++){
+      if(((i*dx-L/2.0)*(i*dx-L/2.0)+(j*dy-L/2.0)*(j*dy-L/2.0))<=r*r){
+        presente[j][i]=100.0;
+        futuro[j][i]=presente[j][i];
+      }
+      else{
+        presente[j][i]=10.0;
+        futuro[j][i]=presente[j][i];
+      }
+    }
+  }
 
-//Primera iteracion
-   for(int t=0;t<1.0/dt;t++){
-     for(int j=0;j<N;j++){
-       for(int i=0;i<N;i++){
-         if(((x[i] - 250) * (x[i] - 250) + (y[j] - 250) * (y[j] - 250)) > r * r){
-           futuro[j][i]=(dt*v)/(1.0*1.0)*(presente[j+1][i]+presente[j-1][i]+presente[j][i+1]+presente[j][i-1]-4.0*presente[j][i])+presente[j][i];
-         }
-       }
-     }
-     for(int j=0;j<N;j++){
-       for(int i=0;i<N;i++){
-         presente[j][i]=futuro[j][i];
-       }
-     }
-   }
+//Derivadas
+  for(int t=0;t<20000.0/dt;t++){
+    for(int j=1;j<N-1;j++){
+      for(int i=1;i<N-1;i++){
+        if(((i*dx-L/2.0)*(i*dx-L/2.0)+(j*dy-L/2.0)*(j*dy-L/2.0))>r*r){
+          futuro[j][i]=((dt*v)/(dx*dx))*(presente[j+1][i]+presente[j-1][i]+presente[j][i+1]+presente[j][i-1]-4.0*presente[j][i])+presente[j][i];
+        }
+        else{
+          futuro[j][i]=100.0;
+        }
+      }
+    }
+    for(int j=0;j<N;j++){
+      for(int i=0;i<N;i++){
+        presente[j][i]=futuro[j][i];
+      }
+    }
+  }
 
 //Salida de datos.
    ofstream datos;
